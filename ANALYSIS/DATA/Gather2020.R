b2956307d065b0843ff96afcea5068082ec52256
RafaelSchuetz <RafaelSchuetz@users.noreply.github.com>
2019-11-20 21:41:40 +0100

    Created folder structure and example files

---
+++ ANALYSIS/DATA/Gather2020.R
@@ -0,0 +1,7 @@
+# This file gathers and cleans CHILDREN survey data for the year 2019, collected with the applications for grants in 2020
+
+# Load packages
+
+library(readxl)
+
+data2020 <- read_excel(./ANALYSIS/DATA/"CHILDREN Wirkungsdaten_VERTRAULICH_final.xlsx", sheet = "2020")
