3e84670695e42ba2c62c6419c66f6ebac4d33576
Laura Jepsen <jepsen.laura@t-online.de>
2019-11-25 12:44:31 +0100

    commit from RStudio

---
+++ ANALYSIS/DATA/2014_2015.R
@@ -0,0 +1,3 @@
+library(readxl)
+daten2014 <- read_excel("ANALYSIS/DATA/CHILDREN Wirkungsdaten_VERTRAULICH_final.xlsx", sheet = "2014")
+daten2015 <- read_excel("ANALYSIS/DATA/CHILDREN Wirkungsdaten_VERTRAULICH_final.xlsx", sheet = "2015")
