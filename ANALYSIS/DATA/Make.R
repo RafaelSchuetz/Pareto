cec0e1c37c13e4ed62d825177281c4cadd70de93
RafaelSchuetz <RafaelSchuetz@users.noreply.github.com>
2019-11-26 17:38:11 +0100

    Updated Merge and Make files

--- ANALYSIS/DATA/Make.R
+++ ANALYSIS/DATA/Make.R
@@ -1,10 +1,10 @@
 # Gather and cleanup raw data files
 
 source("./ANALYSIS/DATA/Gather2012_2013.R")
-source("./Analysis/Data/2014_2015.R")
-source("./Analysis/Data/Gather2016.R")
-source("./Analysis/Data/Gather2017.R")
-source("./Analysis/Data/Gather2020.R")
+source("./ANALYSIS/DATA/2014_2015.R")
+source("./ANALYSIS/DATA/Wirkungsdaten 2016.R")
+source("./ANALYSIS/DATA/Wirkungsdaten 2017.R")
+source("./ANALYSIS/DATA/Gather2020.R")
 
 # Merge cleaned and augmented data frames into data.frames "lnRealUSFundAnn19502018", "realUSOutp19502018" and "USRecMonth"
 
