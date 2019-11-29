035f9e0808118be403e310479efb2a920353ff03
RafaelSchuetz <RafaelSchuetz@users.noreply.github.com>
2019-11-29 15:09:27 +0100

    Sourced data from 2018 and 2019

--- ANALYSIS/DATA/Make.R
+++ ANALYSIS/DATA/Make.R
@@ -6,6 +6,8 @@ source(encoding = "UTF8", "./ANALYSIS/DATA/Gather2012_2013.R")
 source(encoding = "UTF8", "./ANALYSIS/DATA/2014_2015.R")
 source(encoding = "UTF8", "./ANALYSIS/DATA/Wirkungsdaten 2016.R")
 source(encoding = "UTF8", "./ANALYSIS/DATA/Wirkungsdaten 2017.R")
+source(encoding = "UTF8", "./ANALYSIS/DATA/2018.R")
+source(encoding = "UTF8", "./ANALYSIS/DATA/2019.R")
 source(encoding = "UTF8", "./ANALYSIS/DATA/Gather2020.R")
 
 # Merge cleaned and augmented data frames into data.frames "lnRealUSFundAnn19502018", "realUSOutp19502018" and "USRecMonth"
