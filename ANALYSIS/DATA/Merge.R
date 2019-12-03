3928141f61067ee24481dc334ac814c072f7933c
Yannick Zurl <yannick@Yannicks-MacBook-Air.local>
2019-12-03 16:40:13 +0100

    Änderung Dateiname

--- ANALYSIS/DATA/Merge.R
+++ ANALYSIS/DATA/Merge.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 # Join data from 2011 to 2019
 
-data201119 <- df_2018 %>% #data2019 %>%
+data201118 <- df_2018 %>% #data2019 %>%
   #full_join(df_2018) %>% 
   full_join(df_2017) %>% 
   full_join(data2016) %>%
