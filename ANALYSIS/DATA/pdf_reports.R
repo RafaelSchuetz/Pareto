ecb103e86399b72a104767b252675e2225e1b52b
Laura Jepsen <jepsen.laura@t-online.de>
2019-12-03 16:18:06 +0100

    pdf reports

---
+++ ANALYSIS/DATA/pdf_reports.R
@@ -0,0 +1,5 @@
+library(pdftools)
+report2016 <- pdf_text("ANALYSIS/DATA/CHILDREN_Jahresbericht_2016.pdf")
+report2017 <- pdf_text("ANALYSIS/DATA/Children_Jahresbericht_2017.pdf")
+
+view(report2016)
