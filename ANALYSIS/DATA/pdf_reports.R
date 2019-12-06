56c3d549845e8228701497dfb7bfa2308a868331
Laura Jepsen <jepsen.laura@t-online.de>
2019-12-06 11:51:25 +0100

    states

--- ANALYSIS/DATA/pdf_reports.R
+++ ANALYSIS/DATA/pdf_reports.R
@@ -22,6 +22,48 @@ subsidy2017 <- subsidy2017 %>% add_column(year = 2017)
 
 subsidy2017$subsidy <- as.numeric(subsidy2017$subsidy)
 
+library(dplyr)
+subsidy2016 <- subsidy2016 %>%
+  mutate('states')
+
+subsidy2016 <- subsidy2016 %>% 
+  dplyr::rename(state = '"states"')
+
+subsidy2017 <- subsidy2017 %>%
+  mutate('states')
+
+subsidy2017 <- subsidy2017 %>% 
+  dplyr::rename(state = '"states"')
+
+subsidy2016[1,5]<- "NRW"
+subsidy2016[2,5]<- "Sachsen"
+subsidy2016[3,5]<- "Bayern"
+subsidy2016[4,5]<- "NRW" 
+subsidy2017[5,5]<- "Berlin"
+subsidy2016[6,5]<- "Berlin" 
+subsidy2016[7,5]<- "Berlin" 
+subsidy2016[8,5]<- "Berlin" 
+subsidy2016[9,5]<- "NRW" 
+subsidy2016[10,5]<- "NRW" 
+subsidy2016[11,5]<- "Niedersachen"
+subsidy2016[12,5]<- "Bremen"
+subsidy2016[13,5]<- "NRW"
+subsidy2016[14,5]<- "NRW"
+subsidy2016[15,5]<- "Sachsen"
+subsidy2016[16,5]<- "Sachsen"
+subsidy2016[17,5]<- "NRW"
+subsidy2016[18,5]<- "NRW"
+subsidy2016[19,5]<- "Brandenburg"
+subsidy2016[20,5]<- "Thüringen"
+subsidy2016[21,5]<- "NRW"
+subsidy2016[22,5]<- "NRW"
+subsidy2016[23,5]<- "Hessen"
+subsidy2016[24,5]<- "Hessen"
+subsidy2016[25,5]<- "Hessen"
+subsidy2016[26,5]<- "Sachsen Anhalt"
+subsidy2016[27,5]<- "Hamburg"
+subsidy2016[28,5]<- "Hamburg"
+subsidy2016[29,5]<- "Niedersachsen"
 
 
 
