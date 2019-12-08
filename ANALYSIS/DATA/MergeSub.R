f1d6531a4b0aea74868642a4afa1b0e74aefe94c
Laura Jepsen <jepsen.laura@t-online.de>
2019-12-08 12:34:00 +0100

    ...

--- ANALYSIS/DATA/MergeSub.R
+++ ANALYSIS/DATA/MergeSub.R
@@ -4,6 +4,8 @@ withSub2016 <- data2016 %>%
 withSub2017 <- df_2017 %>%
   full_join(subsidy2017) 
 
+#joinwithstatedata
+
 dataWithStatesAndSub <- dataWithState %>%
   full_join(subsidy2016) %>%
   full_join(subsidy2017)
