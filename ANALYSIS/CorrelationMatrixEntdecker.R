49cda5756a2f1fb5257defdc83addd7ab0f56ab1
Jonathan Kirschner <58041502+jook-a@users.noreply.github.com>
2019-12-09 12:33:38 +0100

    more details for cormatrix

--- ANALYSIS/CorrelationMatrixEntdecker.R
+++ ANALYSIS/CorrelationMatrixEntdecker.R
@@ -15,7 +15,7 @@ corTrips <- subset(data201118, select = c("tripsSuggestions", "tripsDecisions", 
                                        "tripsNo", "tripsKidsNo", "tripsTotalCost", "tripsDifferentKidsNo",
                                        "tripsSubsidyRequest", "tripsSubsidy"))
 
-#create correlation matrix with missing values included
+#create a simple correlation matrix with missing values included
 
 
 cor(corTrips, method = "pearson", use = "complete.obs")
@@ -23,4 +23,48 @@ cor(corTrips, method = "pearson", use = "complete.obs")
 #method: indicates the correlation coefficient to be computed. 
 #The default is pearson correlation coefficient which measures the linear dependence between two variables.
 
+
+# Create Matrix with p-values (significance levels) ---------------------------------------------
+
+#install.packages("Hmisc")
+
+library("Hmisc")
+corTripsSign <- rcorr(as.matrix(corTrips))
+
+# Extract the correlation coefficients
+corTripsSign$r
+# Extract p-values
+corTripsSign$P
+
+#Note:The output of the function rcorr() is a list containing the following elements : 
+#- r : the correlation matrix 
+#- n : the matrix of the number of observations used in analyzing each pair of variables 
+#- P : the p-values corresponding to the significance levels of correlations. 
+
+
+
+# better overview ---------------------------------------------------------
+
+#Use symnum() function: Symbolic number coding
+
+#The R function symnum() replaces correlation coefficients by symbols according to the level of the correlation. 
+#It takes the correlation matrix as an argument :
+
+symnum(corTrips, cutpoints = c(0.3, 0.6, 0.8, 0.9, 0.95),
+       symbols = c(" ", ".", ",", "+", "*", "B"),
+       abbr.colnames = TRUE)
+?symnum
+
+#error: "benoetige 2 symbols fuer boolesches 'x' argument
+
+#Draw a correlogram
+
+#install.packages("corrplot")
+
+library(corrplot)
+corrplot(corTrips, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+?corrplot
+#Error in matrix(if (is.null(value)) logical() else value, nrow = nr, dimnames = list(rn,  : 
+#length of 'dimnames' [2] not equal to array extent
 
--- ANALYSIS/CorrelationMatrixMittagstisch.R
+++ ANALYSIS/CorrelationMatrixMittagstisch.R
@@ -28,7 +28,7 @@ corMT <- subset(data201118, select = c("eatersPerMealNo", "newKidsNo", "catering
 #note: DGECriteriaBinary and recipesBinary are excluded, Fehlermeldung "keine vollständigen Elementpaare" 
 
 
-#create correlation matrix with missing values included
+#create a simple correlation matrix with missing values included
 
 
 cor(corMT, method = "pearson", use = "complete.obs")
@@ -36,5 +36,46 @@ cor(corMT, method = "pearson", use = "complete.obs")
 #method: indicates the correlation coefficient to be computed. 
 #The default is pearson correlation coefficient which measures the linear dependence between two variables.
 
+# Create Matrix with p-values (significance levels) ---------------------------------------------
+
+#install.packages("Hmisc")
+
+library("Hmisc")
+corMTSign <- rcorr(as.matrix(corMT))
+
+# Extract the correlation coefficients
+corMTSign$r
+# Extract p-values
+corMTSign$P
+
+#Note:The output of the function rcorr() is a list containing the following elements : 
+#- r : the correlation matrix 
+#- n : the matrix of the number of observations used in analyzing each pair of variables 
+#- P : the p-values corresponding to the significance levels of correlations. 
 
 
+
+# better overview ---------------------------------------------------------
+
+#Use symnum() function: Symbolic number coding
+
+#The R function symnum() replaces correlation coefficients by symbols according to the level of the correlation. 
+#It takes the correlation matrix as an argument :
+
+symnum(corMT, cutpoints = c(0.3, 0.6, 0.8, 0.9, 0.95),
+       symbols = c(" ", ".", ",", "+", "*", "B"),
+       abbr.colnames = TRUE)
+?symnum
+
+#error: "benoetige 2 symbols fuer boolesches 'x' argument
+
+#Draw a correlogram
+
+#install.packages("corrplot")
+
+library(corrplot)
+corrplot(corMT, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+?corrplot
+#Error in matrix(if (is.null(value)) logical() else value, nrow = nr, dimnames = list(rn,  : 
+#length of 'dimnames' [2] not equal to array extent
