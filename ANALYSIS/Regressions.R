547bba7e40121633aeaccf69e20a68e5eeeb4d98
Laura Jepsen <jepsen.laura@t-online.de>
2020-01-11 22:24:06 +0100

    alte plots raus

--- ANALYSIS/Regressions.R
+++ ANALYSIS/Regressions.R
@@ -4,13 +4,6 @@ library(VGAM)
 
 view(mergedData)
 
-plot(mergedData$DGECriteriaNo, mergedData$lessIll, xlab="lessIll", ylab="DGE", col="darkblue", main="DGE_lessIll")
-plot(mergedData$DGECriteriaNo, mergedData$dietaryKnowledge, xlab="dietaryKnowledge", ylab="DGE", col="darkblue", main="DGE_dietaryKnowledge")
-plot(mergedData$DGECriteriaNo, mergedData$seasonalFoodstuff, xlab = "seasonalFood", ylab="DGE",col="darkblue", main = "DGE_seasonalFood")
-plot(mergedData$DGECriteriaNo, mergedData$organicFoodstuff, xlab = "organicFood", ylab= "DGE",col="darkblue", main = "DGE_organicFood")
-plot(mergedData$DGECriteriaNo, mergedData$appreciateHealthy, xlab = "appreciateHealthy", ylab= "DGE", col="darkblue", main = "DGE_appreciateHealthy")
-plot(mergedData$DGECriteriaNo, mergedData$tasksLunch, xlab= "tasksLunch", ylab = "DGE",col="darkblue", main= "DGE_tasksLunch")
-
 ##proportionalOddsModel 
 
 DGE_lessIll.vglm = vglm(lessIll ~  DGECriteriaNo, data = mergedData, family = propodds)
