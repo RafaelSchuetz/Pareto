4f6454a7f551b888cfd06b6ea921a3059af55046
Laura Jepsen <jepsen.laura@t-online.de>
2020-01-17 16:27:43 +0100

    fghjkl

--- ANALYSIS/DATA/MakeReal.R
+++ ANALYSIS/DATA/MakeReal.R
@@ -4,4 +4,5 @@ mergedData <- mergedData %>%
   mutate(realSubsidy = subsidy/foodPriceIndex*100,
          realSubsidyRequest = subsidyRequest/foodPriceIndex*100,
          realTripsSubsidy = tripsSubsidy/tripsPriceIndex*100,
-         realTripsSubsidyRequest = tripsSubsidyRequest/tripsPriceIndex*100)
+         realTripsSubsidyRequest = tripsSubsidyRequest/tripsPriceIndex*100,
+         realTotalCosts = totalCost/foodPriceIndex*100)
