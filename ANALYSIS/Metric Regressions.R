eb6a9cef0bc0d3009bd2726ea4c5d72a0e90de59
Laura Jepsen <jepsen.laura@t-online.de>
2020-02-20 13:57:27 +0100

    oiihjoi

--- ANALYSIS/Metric Regressions.R
+++ ANALYSIS/Metric Regressions.R
@@ -75,6 +75,8 @@ summary(tripsNo_sub_scaled_IM.lm) #standardized
 
 #dataset: mergedData
 
+#standardized coefficents
+
 mergedData$lessIll_scaled<-scale(mergedData$lessIll)
 mergedData$DGECriteriaNo_scaled<-scale(mergedData$DGECriteriaNo)
 mergedData$regionalProducts_scaled<-scale(mergedData$regionalProducts)
@@ -128,6 +130,9 @@ mergedDataImputeMode$dietaryKnowledge_scaled<-scale(mergedDataImputeMode$dietary
 lessIll_DGE_IM.lm <- lm(lessIll ~ DGECriteriaNo, data = mergedDataImputeMode)
 summary(lessIll_DGE_IM.lm)
 
+lessIll_DGE_IM_scaled.lm <- lm(lessIll_scaled ~ DGECriteriaNo_scaled, data = mergedDataImputeMode)
+summary(lessIll_DGE_IM_scaled.lm) #standardized
+
 lm.beta(lessIll_DGE_IM.lm) #standardized
 
 Expand_LessIll_IM.lm = lm(lessIll ~ DGECriteriaNo + regionalProducts + yearsSupportSince + realSubsidy + state, data = mergedDataImputeMode)
@@ -135,39 +140,70 @@ summary(Expand_LessIll_IM.lm)
 
 lm.beta(Expand_LessIll_IM.lm) #standardized
 
+Expand_LessIll_IM_scaled.lm = lm(lessIll_scaled ~ DGECriteriaNo_scaled + regionalProducts_scaled + yearsSupportSince + realSubsidy_scaled + state, data = mergedDataImputeMode)
+summary(Expand_LessIll_IM_scaled.lm) #standardized
+
 dietaryKnowledge_DGE_IM.lm <- lm(dietaryKnowledge ~ DGECriteriaNo, data = mergedDataImputeMode)
 summary(dietaryKnowledge_DGE_IM.lm)
 
 lm.beta(dietaryKnowledge_DGE_IM.lm) #standardized
+
+dietaryKnowledge_DGE_IM_scaled.lm <- lm(dietaryKnowledge_scaled ~ DGECriteriaNo_scaled, data = mergedDataImputeMode)
+summary(dietaryKnowledge_DGE_IM_scaled.lm) #standardized
 
 appreciateHealthy_DGE_IM.lm <- lm(appreciateHealthy ~ DGECriteriaNo, data = mergedDataImputeMode)
 summary(dietaryKnowledge_DGE_IM.lm)
 
 lm.beta(appreciateHealthy_DGE_IM.lm) #standardized
 
+appreciateHealthy_DGE_IM_scaled.lm <- lm(appreciateHealthy_scaled ~ DGECriteriaNo_scaled, data = mergedDataImputeMode)
+summary(dietaryKnowledge_DGE_IM_scaled.lm) #standardized
+
 ###chance equality
 ##dataset: mergedData 
+
+#standardized coefficents 
+
+mergedData$selfworth_scaled<-scale(mergedData$selfworth)
+mergedData$dayToDaySkills_scaled<-scale(mergedData$dayToDaySkills)
 
 selfworth.lm <- lm(selfworth ~ realSubsidy, data = mergedData)
 summary(selfworth.lm)
 
 lm.beta(selfworth.lm) #standardized
 
+selfworth_scaled.lm <- lm(selfworth_scaled ~ realSubsidy_scaled, data = mergedData)
+summary(selfworth_scaled.lm) #standardized
+
 dayToDaySkills.lm <- lm(dayToDaySkills ~ realSubsidy, data = mergedData)
 summary(dayToDaySkills.lm)
 
 lm.beta(dayToDaySkills.lm) #standardized
+
+dayToDaySkills_scaled.lm <- lm(dayToDaySkills_scaled ~ realSubsidy_scaled, data = mergedData)
+summary(dayToDaySkills_scaled.lm)
  
 ##dataset: mergedDataImputeMode
+
+#standardized coefficents
+
+mergedDataImputeMode$selfworth_scaled<-scale(mergedDataImputeMode$selfworth)
+mergedDataImputeMode$dayToDaySkills_scaled<-scale(mergedDataImputeMode$dayToDaySkills)
 
 selfworth_IM.lm <- lm(selfworth ~ realSubsidy, data = mergedDataImputeMode)
 summary(selfworth_IM.lm)
 
 lm.beta(selfworth_IM.lm) #standardized
 
+selfworth_IM_scaled.lm <- lm(selfworth_scaled ~ realSubsidy_scaled, data = mergedDataImputeMode)
+summary(selfworth_IM_scaled.lm) #standardized
+
 dayToDaySkills_IM.lm <- lm(dayToDaySkills ~ realSubsidy, data = mergedDataImputeMode)
 summary(dayToDaySkills_IM.lm)
 
 lm.beta(dayToDaySkills_IM.lm) #standardized
+
+dayToDaySkills_IM_scaled.lm <- lm(dayToDaySkills_scaled ~ realSubsidy_scaled, data = mergedDataImputeMode)
+summary(dayToDaySkills_IM_scaled.lm)
 
 
