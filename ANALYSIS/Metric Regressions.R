92ad9469233f46009a11b76b6a744a782635e829
Laura Jepsen <jepsen.laura@t-online.de>
2020-02-20 13:40:09 +0100

    lokök

--- ANALYSIS/Metric Regressions.R
+++ ANALYSIS/Metric Regressions.R
@@ -1,6 +1,7 @@
 #OLS regressions & their standardized coefficents
 
 library(QuantPsyc)
+library(Scale)
 
 #standardized coefficents function 
 
@@ -17,54 +18,113 @@ function (MOD)
 ##subsidy 
 #dataset: mergedData
 
+#standardizedcoefficents
+
+mergedData$mealsNo_scaled<-scale(mergedData$mealsNo)
+mergedData$realSubsidy_scaled<-scale(mergedData$realSubsidy)
+mergedData$tripsNo_scaled<-scale(mergedData$tripsNo)
+mergedData$realTripsSubsidy_scaled<-scale(mergedData$realTripsSubsidy)
+
+#regressions
+
 mealsNo_sub.lm <- lm(mealsNo ~ realSubsidy, data = mergedData)
 summary(mealsNo_sub.lm)
 
 lm.beta(mealsNo_sub.lm) #standardized
+
+mealsNo_sub_scaled.lm <- lm(mealsNo_scaled ~ realSubsidy_scaled, data = mergedData)
+summary(mealsNo_sub_scaled.lm) #standardized
 
 tripsNo_sub.lm <- lm(tripsNo ~ realTripsSubsidy, data = mergedData)
 summary(tripsNo_sub.lm)
 
 lm.beta(tripsNo_sub.lm) #standardized
 
+tripsNo_sub_scaled.lm <- lm(tripsNo_scaled ~ realTripsSubsidy_scaled, data = mergedData)
+summary(tripsNo_sub_scaled.lm) #standardized
+
 #datset: mergedDataImputeMode
+
+#standardizedcoefficents
+
+mergedDataImputeMOde$mealsNo_scaled<-scale(mergedDataImputeMode$mealsNo)
+mergedDataImputeMode$realSubsidy_scaled<-scale(mergedDataImputeMode$realSubsidy)
+mergedDataImputeMode$tripsNo_scaled<-scale(mergedDataImputeMode$tripsNo)
+mergedDataImputeMode$realTripsSubsidy_scaled<-scale(mergedDataImputeMode$realTripsSubsidy)
+
+#regressions 
 
 mealsNo_sub_IM.lm <- lm(mealsNo ~ realSubsidy, data = mergedDataImputeMode)
 summary(mealsNo_sub.lm)
 
 lm.beta(mealsNo_sub_IM.lm) #standardized
 
+mealsNo_sub_scaled_IM.lm <- lm(mealsNo_scaled ~ realSubsidy_scaled, data = mergedDataImputeMode)
+summary(mealsNo_sub_scaled_IM.lm) #standardized
+
 tripsNo_sub_IM.lm <- lm(tripsNo ~ realTripsSubsidy, data = mergedDataImputeMode)
 summary(tripsNo_sub.lm)
 
 lm.beta(tripsNo_sub_IM.lm) #standardized
+
+tripsNo_sub_scaled_IM.lm <- lm(tripsNo_scaled ~ realTripsSubsidy_scaled, data = mergedDataImputeMode)
+summary(tripsNo_sub_scaled_IM.lm) #standardized
 
 
 ##healthinfluence 
 
 #dataset: mergedData
 
+mergedData$lessIll_scaled<-scale(mergedData$lessIll)
+mergedData$DGECriteriaNo_scaled<-scale(mergedData$DGECriteriaNo)
+mergedData$regionalProducts_scaled<-scale(mergedData$regionalProducts)
+mergedData$dietaryKnowledge_scaled<-scale(mergedData$dietaryKnowledge)
+
+#regressions
+
 lessIll_DGE.lm <- lm(lessIll ~ DGECriteriaNo, data = mergedData)
 summary(lessIll_DGE.lm)
 
 lm.beta(lessIll_DGE.lm) #standardized
+
+lessIll_DGE_scaled.lm <- lm(lessIll_scaled ~ DGECriteriaNo_scaled, data = mergedData)
+summary(lessIll_DGE.lm) #standardized
 
 Expand_LessIll.lm = lm(lessIll ~ DGECriteriaNo + regionalProducts + yearsSupportSince + realSubsidy + state, data = mergedData)
 summary(Expand_LessIll.lm)
 
 lm.beta(Expand_LessIll.lm) #standardized
 
+Expand_LessIll_scaled.lm = lm(lessIll_scaled ~ DGECriteriaNo_scaled + regionalProducts_scaled + yearsSupportSince + realSubsidy_scaled + state, data = mergedData)
+summary(Expand_LessIll_scaled.lm) #standardized
+
 dietaryKnowledge_DGE.lm <- lm(dietaryKnowledge ~ DGECriteriaNo, data = mergedData)
 summary(dietaryKnowledge_DGE.lm)
 
 lm.beta(dietaryKnowledge_DGE.lm) #standardized
+
+dietaryKnowledge_DGE_scaled.lm <- lm(dietaryKnowledge_scaled ~ DGECriteriaNo_scaled, data = mergedData)
+summary(dietaryKnowledge_DGE_scaled.lm) #standardized
 
 appreciateHealthy_DGE.lm <- lm(appreciateHealthy ~ DGECriteriaNo, data = mergedData)
 summary(dietaryKnowledge_DGE.lm)
 
 lm.beta(appreciateHealthy_DGE.lm) #standardized
 
+appreciateHealthy_DGE_scaled.lm <- lm(appreciateHealthy_scaled ~ DGECriteriaNo_scaled, data = mergedData)
+summary(dietaryKnowledge_DGE_scaled.lm) #standardized
+
 #dataset: mergedDataImputeMode
+
+#standardized coefficents
+
+mergedDataImputeMode$lessIll_scaled<-scale(mergedDataImputeMode$lessIll)
+mergedDataImputeMode$DGECriteriaNo_scaled<-scale(mergedDataImputeMode$DGECriteriaNo)
+mergedDataImputeMode$regionalProducts_scaled<-scale(mergedDataImputeMode$regionalProducts)
+mergedDataImputeMode$dietaryKnowledge_scaled<-scale(mergedDataImputeMode$dietaryKnowledge)
+
+#regressions
+
 lessIll_DGE_IM.lm <- lm(lessIll ~ DGECriteriaNo, data = mergedDataImputeMode)
 summary(lessIll_DGE_IM.lm)
 
