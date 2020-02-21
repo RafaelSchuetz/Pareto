3bd05f5b0a5aa16157cb85720b5b36108539069d
huberlaura <58167103+huberlaura@users.noreply.github.com>
2020-02-21 17:16:22 +0100

    regresiontable

---
+++ regression_table.R
@@ -0,0 +1,18 @@
+#Regressionstable erstellen
+
+library(stargazer)
+#Hlavac, Marek (2018). stargazer: Well-Formatted Regression and Summary Statistics Tables.
+#R package version 5.2.2. https://CRAN.R-project.org/package=stargazer 
+
+lm1_selfworth <- lm(dfcEF$selfworth ~ dfcEF$treatEF)
+
+lm1_skills <- lm(dfcEF$dayToDaySkills ~ dfcEF$treatEF)
+
+
+
+
+stargazer(lm1_selfworth, lm1_skills,
+          title = "Results",
+          align = TRUE,
+          type = 'text')
+
