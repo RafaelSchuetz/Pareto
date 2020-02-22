27214c10143c570f1770ecdee1b8f43c213e05b7
Yannick Zurl <yannick@Yannicks-MacBook-Air.local>
2020-02-22 10:52:07 +0100

    Controls

--- ANALYSIS/DATA/control_variables.R
+++ ANALYSIS/DATA/control_variables.R
@@ -8,6 +8,44 @@
 
 ### Ermitteln relevanter zeit-varianter Kontrollvariablen
 
-# Erstellen einer Korrelationsmatrix
+
+# Methode: Korrelation zwischen den relevanten Variablen und möglichen Kontrollvariablen aus
+# den Mittagstisch Outcomes
+
+
+
+#Aim: Correlation Matrix of Mittagstisch Outcomes
+#data of interest: data201118 to include only numeric variables
+#show cor between complete observations to include missing values
+
+
+# Outcomes Mittagstisch ---------------------------------------------------
+
+
+#1. Correlation matrix: Outcomes Mittagstisch
+#Create subset including the outcomes of interest:
+
+corMT <- subset(dfcEF, select = c("eatersPerMealNo", "newKidsNo", "cateringNo",
+                                       "mealsInInstitutionNo", "mealsNo",  "breakfastsNo",
+                                       "lunchesNo", "snacksNo", "dinnersNo", "offersPerWeekNo",
+                                       "weeksOfferedNo", "daysOfferedNo", "DGECriteriaNo", "totalCost",
+                                       "subsidyRequest", "subsidy", "migrantBackgroundShare", "refugeesShare",
+                                       "unemploymentShare", "multipleChildrenHouseholdShare", "singleParentShare",
+                                       "povertyShare", "participateMore", "tasksLunch", "monthlyCooks", "weeklyCooks",
+                                       "shoppers", "ownIdeas", "stayLonger", "easyDishes", "dietaryKnowledge",
+                                       "appreciateHealthy", "foodCulture", "influenceHome", "cookAtHome", "askRecipes",
+                                       "moreConcentrated", "moreBalanced", "lessIll", "moreIndependent",
+                                       "betterTeamwork", "betterReading", "betterNumbers", "betterGrades",
+                                       "moreRegularSchoolVisits", "moreOpen", "moreConfidence",
+                                       "addressProblems", "proud", "success", "abiturNo", "mittlereReifeNo", "hauptschuleNo",
+                                       "noDegreeNo", "trainingStartedNo", "trainingCompletedNo", "enoughFood",
+                                       "enoughStaffLunch", "enoughStaffActivities", "qualitySatisfies", "regionalProducts",
+                                       "cultureReligion", "unsweetenedDrinks"))
+
+cor(dfcEF$selfworth, corMT, method = c("pearson", "kendall", "spearman"), use = "complete.obs")
+
+# Day-To-Day-Skills als Zielvariable
+lm1_skills <- lm_robust(dfcEF$dayToDaySkills ~ dfcEF$treatEF + dfcEF$subsidy)
+summary(lm1_skills)
 
 
