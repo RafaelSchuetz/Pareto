8faf73b612c24e88b9b6134b481b6c0ce2fe6151
Yannick Zurl <yannick@Yannicks-MacBook-Air.local>
2020-02-22 10:38:48 +0100

    controls

--- ANALYSIS/DATA/control_variables.R
+++ ANALYSIS/DATA/control_variables.R
@@ -1,2 +1,13 @@
-# Kontrollvariablen für die DiD-Estimations
+### Kontrollvariablen für die DiD-Estimations
+
+# Die id-fixed effects kontrollieren bereits für alle zeit-invarianten Eigenschaften, die 
+# zwischen den Einrichtungen variieren (z.B. Bundesland, Große / Kleine Stadt). Die time-fixed
+# effects kontrollieren für Eigenschaften, die über die Zeit variieren, aber für die Einrichtungen
+# konstant sind (z.B. allgemeiner Zeittrend). Aus diesem Grund werden nur Kontrollvariablen 
+# ermittelt, die über die Zeit variieren.
+
+### Ermitteln relevanter zeit-varianter Kontrollvariablen
+
+# Erstellen einer Korrelationsmatrix
+
 
