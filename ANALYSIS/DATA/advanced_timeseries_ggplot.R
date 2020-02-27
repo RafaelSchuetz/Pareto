d0f2c52dc50ae75f1374ea115245a82bc1a9cab5
RafaelSchuetz <rafael.schuetz@gmail.com>
2020-02-27 10:40:38 +0100

    convert time series to ggplot

--- ANALYSIS/DATA/advanced_timeseries_ggplot.R
+++ ANALYSIS/DATA/advanced_timeseries_ggplot.R
@@ -1,15 +1,19 @@
+library(ggfortify)
 
+df_dayToDaySkills_treat <- fortify(ts_dayToDaySkills_treat)
 # ts to data.frame --------------------------------------------------------
 
 #für dayToDaySkills:
 
-ts_reshape(ts_dayToDaySkills_treat, type = "wide", frequency = NULL)
+# ts_reshape(ts_dayToDaySkills_treat, type = "wide", frequency = NULL)
 
 #Error in ts_reshape(ts_dayToDaySkills_treat, type = "wide", frequency = NULL) : 
 #The frequency of the series is invalid, the function support only 'weekly', 'monthly' or 'quarterly' frequencies
 
-data.frame(Y=as.matrix(ts_dayToDaySkills_treat), date=time(ts_dayToDaySkills_treat))
+df_dayToDaySkills_treat <- data.frame(Y=as.matrix(ts_dayToDaySkills_treat), date=time(ts_dayToDaySkills_treat))
 
-class(ts_dayToDaySkills_treat)
+# class(df_dayToDaySkills_treat)
 
 #immernoch ts
+
+plot_dayToDaySkills_treat <- ggplot(df_dayToDaySkills_treat, aes(Index, Data)) + geom_line()
