2adbd5aeab5f63fa13b753af3dad892fc1d8018d
Yannick Zurl <yannick@Yannicks-Air.fritz.box>
2020-03-15 20:30:47 +0100

    UPDATE

--- ANALYSIS/data_example_presentation.R
+++ ANALYSIS/data_example_presentation.R
@@ -3,11 +3,20 @@ library(dplyr)
 dataPraesi <- mergedData %>% filter(id == "103" | id == "112") %>% 
   dplyr::select(id, year, eatersPerMealNo, selfworth, tripsNo)
 
-dataPraesi <- dataPraesi[-c(1, 2, 3, 7, 8, 9, 10, 11, 15), ]
+dataPraesi <- dataPraesi[-c(1, 2, 3, 7, 8, 9, 10, 11, 15, 16), ]
 
 library(xtable)
 
-dataExample <- print(xtable(dataPraesi), include.rownames=FALSE)
-
+dataExample <- xtable(dataPraesi)
 
 saveRDS(dataExample, "./ANALYSIS/GRAPHS/PAPER/dataExample.Rds")
+
+print(xtable(dataExample, include.rownames = FALSE,
+             caption = "Beispielhafter Datensatz",
+             align = "lccccc"))
+
+
+print(xtable(dataExample), include.rownames = FALSE,
+      caption = "Beispielhafter Datensatz",
+      align = "lccccc",
+      label = "fundamentalDynamics")
